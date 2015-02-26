3c867c1badb26e6f715e22a5377fa40251298291
Ian Brennan <ian.brennan@icloud.com>
2015-02-26 13:54:52 -0700

    made events table

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'events#index'
+  resources :events, only: [:new]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
