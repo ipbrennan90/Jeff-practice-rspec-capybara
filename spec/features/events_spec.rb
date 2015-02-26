a4ec10e580e891ac1f8de97414ef7013a832b4bf
Ian Brennan <ian.brennan@icloud.com>
2015-02-26 13:21:10 -0700

    created events controller

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -1,1 +1,19 @@
 require 'rails_helper'
+
+feature 'Events' do
+
+  scenario 'User creates new Event' do
+
+    visit root_path
+    click_on 'New Event'
+    fill_in 'Event Name', with: 'event name'
+    fill_in 'Description', with: 'event description'
+    fill_in 'Date', with: '2015-03-05'
+    click_on 'Create Event'
+
+    expect(page).to have_content('event name')
+    expect(page).to have_content('event description')
+    expect(page).to have_content('2015-03-05')
+
+  end
+end
